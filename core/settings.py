77009589e03f773ec98eaac15479f7cc9e904bda
Harsh <soniverma2006@gmail.com>
2026-04-26 01:54:21 +0530

    Configure for Vercel deployment

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)4q#hayskclgnjpavcv)!wsmr(15g#pe)946q_kh#2e8spcg+g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
